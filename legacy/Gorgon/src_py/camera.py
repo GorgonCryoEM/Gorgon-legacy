51e7da1fca7b586ea5e4d2d0f411eb82cd9517b0
shadow_walker <shadowwalkersb@gmail.com>
2016-08-23 18:42:57 -0500

    fix ImportError
    fix glutInit call
    
    setup.py: fix and update

--- legacy/Gorgon/src_py/camera.py
+++ legacy/Gorgon/src_py/camera.py
@@ -202,7 +202,7 @@ class Camera(QtOpenGL.QGLWidget):
         self.initializeScene()
 
     def initializeScene(self):
-        if((sys.platform != 'darwin') and (sys.platform != 'win32')):
+        if((sys.platform != 'darwin')):
             glutInit(sys.argv)      #This must be here to get it to work with Freeglut.
             #otherwise you get: "freeglut  ERROR:  Function <glutWireCube> called without first calling 'glutInit'."
        
